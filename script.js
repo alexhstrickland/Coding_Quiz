9276dec3cf434ba4352bfd47f29c6a9005681252
Alex Strickland <alexhstrickland@gmail.com>
2020-12-26 23:47:45 -0500

    Function is looping through question correctly and diaplying whether the question is correct

--- script.js
+++ script.js
@@ -11,58 +11,59 @@ var buttonOne = document.querySelector("#btn1");
 var buttonTwo = document.querySelector("#btn2");
 var buttonThree = document.querySelector("#btn3");
 var buttonFour= document.querySelector("#btn4");
-var resultSection = document.querySelector("#result-sec")
+// var resultSection = document.querySelector("#result-sec");
+var scorePage = document.querySelector("#scores");
 
 var questionAnswers = [
     {
         question: "Which of the following are looping structures in JavaScript?",
         options: ["For loop", "While loop", "Do-while loop", "All the above"],
-        answer: "All the above"
+        answer: 4
     },
     {
         question: "Which of the following returns the calling string value converted to lower case?",
         options: ["changeCase(case)", "toLower()", "toLowerCase()", "None of the above"],
-        answer: "toLowerCase()"
+        answer: 3
     },
     {
         question: "Which adds one or more elements to the end of an array and returns the new length of the array?",
         options: ["push()", "map()", "pop()", "join()"],
-        answer: "push()"
+        answer: 1
     },
     {
         question: "What HTML tag is JavaScript written under?",
         options: ["<js>", "<script>","<javascript>", "<scripted>"],
-        answer: "<script>"
+        answer: 2
     },
     {
         question: "Which operator is used to assign a value to a variable?",
         options: ["=", "-", "+", "/"],
-        answer: "="
+        answer: 1
     },
     {
         question: "How do you write 'Hello' in an alert box?",
         options: ["alertBox('Hello');", "msg('Hello');", "msgBox('Hello');", "alert('Hello');"],
-        answer: "alert('Hello');"
+        answer: 4
     },
     {
         question: "How does a for loop begin?",
         options: ["for (i <= 10, i++)", "for i=1 to 10", "for (i=0, i<=5)", "for (i=0; i <=10; i++)"],
-        answer: "for (i=0; i <=10; i++)"
+        answer: 4
     },
     {
         question: "How do you round the number 5.25 to the nearest integer?",
         options: ["rnd(5.25)", "Math.rnd(5.25)", "Math.round(5.25)", "round(5.25)"],
-        answer: "Math.round(5.25)"
+        answer: 3
     },
     {
         question: "Which event occurs when the user clicks on an HTML element?",
         options: ["onchange", "onmouseclick", "onclick", "onmouseover"],
-        answer: "onclick"
+        answer: 3
     },
     {
         question: "Which of the following is not a JavaScript data type?",
         options: ["Number", "Float", "Boolean", "Undefined"],
-        answer: "Float"
+        answer: 2
     }
 ];
 
@@ -115,7 +116,7 @@ function getQuestion(currentQuestion) {
             var newB = document.createElement("button");
 
             newB.classList.add("btn");
-            newB.setAttribute= ("data-value", i+1)
+            newB.setAttribute("data-value", i+1);
             newB.textContent = questionAnswers[currentQuestion].options[i];
 
 
@@ -130,41 +131,48 @@ function getQuestion(currentQuestion) {
 
 
 function questionClick() {
+    var clickedEvent = ""
+    var resultSection = document.querySelector("#result-sec");
+    aBtns.addEventListener("click", function (event) {
+        if (event.target.matches("button")) {
+            clickedEvent = event.target;
+            console.log(clickedEvent);
+        }
+    // })
     // check if user guessed wrong
-    if (this.value !== questions[currentQuestion].answer) {
-      // penalize time
-      time -= 15;
-  
-      if (time < 0) {
-        time = 0;
-      }
-  
-      // display new time on page
-  
-      // play "wrong" sound effect
-  
-      resultSection.textContent = "Wrong!";
-    } else {
-      // play "right" sound effect
-  
-      resultSection.textContent = "Correct!";
-    }
-  
-    // flash right/wrong feedback on page for half a second
-    feedbackEl.setAttribute("class", "feedback");
-    setTimeout(function() {
-      feedbackEl.setAttribute("class", "feedback hide");
-    }, 1000);
-  
-    // move to next question
-  
-    // check if we've run out of questions
-    if (currentQuestionIndex === questions.length) {
-      quizEnd();
-    } else {
-      getQuestion();
-    }
-  }
+        if (parseInt(clickedEvent.getAttribute("data-value")) !== questionAnswers[currentQuestion].answer) {
+        // penalize time
+            time -= 15;
+        
+            if (time < 0) {
+                time = 0;
+            }
+    
+            resultSection.textContent = "Incorrect!";
+            } else {
+
+    
+            resultSection.textContent = "Correct!";
+            score +=;
+            }
+    
+        // flash right/wrong feedback on page for half a second
+        resultSection.setAttribute("class", "feedback");
+        setTimeout(function() {
+        resultSection.setAttribute("class", "feedback hide");
+        }, 1000);
+    
+        // move to next question
+    
+        // check if we've run out of questions
+        if (currentQuestion=== questionAnswers.length) {
+        quizEnd();
+        } else {
+            currentQuestion = currentQuestion + 1;
+            getQuestion(currentQuestion);
+        }
+    })
+}
   
 function quizEnd() {
     // stop timer
@@ -220,6 +228,15 @@ startBtn.addEventListener("click", function() {
     startQuiz();
     currentQuestion = 0;
     getQuestion(0);
+    questionClick();
+    // aBtns.addEventListener("click", function (event) {
+    //     if (event.target.matches("button")) {
+    //         var clickedEvent = event.target;
+    //         console.log(clickedEvent); 
+    //     }
+    // })
+
+    
 });
 
   // user clicks button to submit initials
